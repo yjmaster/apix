3b3012ed13c8ae6a9b661e1503f32df274bcd231
kwonds770 <kwonds770@gmail.com>
2022-06-07 18:24:50 +0900

    test

--- apix.py
+++ apix.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Resource, Api
 from flask_cors import CORS, cross_origin
 
-### ?�플 ###
+### sample ###
 from todo.todo import Todo 
 
 ### GPT API ###
