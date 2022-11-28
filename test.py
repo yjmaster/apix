ef878aebf4f76531f67d6454e2668379eaa9c783
minui2167 <105832345+minui2167@users.noreply.github.com>
2022-11-29 08:06:30 +0900

    delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-text = '''
-500대 기업 기부금 내역 공시한 기업 257곳 중 1635억원 증가.삼성전자·SK하이닉스·LG생활건강·현대차·포스코 등 순.교보생명 기부금 증가액 가장 커...보험사 '자존심' 지켰다
-'''
-from konlpy.tag import Kkma
-kkma = Kkma()
-s = kkma.sentences(text)
-print(s)
