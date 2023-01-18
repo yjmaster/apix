c5d1100d0a4b22d6a6fc8595c397fcdc96cf42bb
kwonds770 <kwonds770@gmail.com>
2023-01-18 15:34:36 +0900

    주제분류 추가

--- controller/service.py
+++ controller/service.py
@@ -4,6 +4,9 @@ from . import kobart_v1
 from kobart_modules.inference2 import kobart_inference_API
 kobart_api = kobart_inference_API()
 
+from classification_modules.inference2 import classification_inference_API
+classification_api = classification_inference_API()
+
 from utils.textFormat import TextFormat
 from utils.customWord import CustomWord
 
@@ -19,7 +22,7 @@ def keyword():
 		content = params['content']
 		summary = params['summary']
 
-		total_contents = (title + '\n' + content)
+		total_contents = (title + ' ' + content)
 		keyword1 = kobart_api.kobart_keyword(total_contents)
 		if not keyword1['success']:
 			res = keyword1
@@ -38,6 +41,27 @@ def keyword():
 		keywords = textFormat.keywordsIncludeContent(total_contents, keywords)
 
 		res["extractor"] = keywords
+
+	except Exception as exp:
+		res = {"success": False, "code": 400, "message": str(exp)}
+	finally:
+		return res
+
+@kobart_v1.route('/topic', methods=['POST'])
+def topic():
+	try:
+		res = {"success": True, "extractor": ""}
+		params = request.get_json()
+		title = params['title']
+		content = params['content']
+
+		total_contents = (title + ' ' + content)
+		topic = classification_api.topic(total_contents)
+		if not topic['success']:
+			res = topic
+			return
+
+		res["extractor"] = topic["extractor"]
 
 	except Exception as exp:
 		res = {"success": False, "code": 400, "message": str(exp)}
