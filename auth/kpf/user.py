a500929ffc6bf04106c6696f75e3e9e481093aa2
kwonds <kwonds770@gmail.com>
2023-05-23 11:27:25 +0900

    로그화면 링크 작업

--- auth/kpf/user.py
+++ auth/kpf/user.py
@@ -33,6 +33,36 @@ class KpfUser:
 			kpfDb.conn.close()
 			return result
 
+	def find_key(self, key):
+		try:
+			kpfDb.connect_db()
+			result = {"success": True}
+
+			_SQL = """SELECT *
+				FROM news_ai_users
+				WHERE 1=1
+				AND id_client = '{key}'""".format(
+					key = key
+				)
+
+			curs = kpfDb.conn.cursor()
+			curs.execute(_SQL)
+			user = curs.fetchone()
+			if user:
+				result["id_client"] = user[0]
+				result["media"] = user[1]
+			else:
+				result["success"] = False
+				result["message"] = "존재하지 않는 사용자 입니다."
+
+		except Exception as exp :
+			result = {"success": False, "message": str(exp)}
+		finally: 
+			kpfDb.conn.cursor().close()
+			kpfDb.conn.close()
+			return result
+
+
 
 
 
--- controller/demo.py
+++ controller/demo.py
@@ -35,11 +35,20 @@ def options():
 @kpf.route('/log', methods=['GET','POST'])
 def log():
     if request.method == 'GET':
+        # http://localhost:10001/log?key=6CF283C1-4708-5BDD-F4D0-B02C8DF03D7E
+        key = request.args.get('key', type=str)
         token = request.cookies.get('access_token')
         if token is None :
-            return render_template('login.html')
+            if key is None:
+                return render_template('login.html')
+            else:
+                isValidkey = kpfUser.find_key(key)
+                if isValidkey["success"]:
+                    return render_template('log.html', accessType = "key")
+                else:
+                    return render_template('login.html')
         else:
-            return render_template('log.html')
+            return render_template('log.html', accessType = "token")
     elif request.method == 'POST':
         data = request.get_json()
         return kpfDb.get_log(data)
